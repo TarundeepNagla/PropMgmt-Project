944ab27402cbc75f4bda05bcf0072d02adc32e86
TSNAGLA <tarundeep.nagla85@gmail.com>
2024-02-24 20:01:02 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
-
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res,next) => {
     const {username, email, password} = req.body;
@@ -31,4 +31,39 @@ export const signup = async (req, res,next) => {
     }
 
 };
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    if (!email || !password || email ==='' || password ===''){
+        next(errorHandler(400, 'All fields are mandatory'));
+
+    }
+
+    try {
+        const validUser = await User.findOne({email});
+        if (!validUser) {
+            return next(errorHandler(404, 'Wrong credentials'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) {
+            return next(errorHandler(400, 'Wrong credentials'));
+        }
+
+        const token = jwt.sign(
+            { id: validUser._id },
+            process.env.JWT_SECRET
+        );
+        //  to sepearate password from rest of the response. (as we are receiving password also as part of response) which should not be the case.
+        const { password: pass, ...rest } = validUser._doc;
+        res
+        .status(200)
+        .cookie('access_token', token, {
+            httpOnly: true,
+        }).json(rest);
+
+    } catch (error) {
+        // res.status(500).json({message: error.message}); Instead of this, will use next() middleware to handle errors. 
+        next(error);
+    }
+}
 
