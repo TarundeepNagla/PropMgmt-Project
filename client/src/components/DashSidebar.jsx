7758e218c44784c56d69213e545d8b18e5385baa
TSNAGLA <tarundeep.nagla85@gmail.com>
2024-02-27 14:04:48 +0530

    complete sidebar of dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,43 @@
+import { Sidebar } from 'flowbite-react';
+import {
+    HiUser,
+    HiArrowSmRight,
+    HiDocumentText,
+    HiOutlineUserGroup,
+    HiAnnotation,
+    HiChartPie,
+} from 'react-icons/hi';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+// import { signoutSuccess } from '../redux/user/userSlice';
+// import { useDispatch } from 'react-redux';
+// import { useSelector } from 'react-redux';
+
+export default function DashSidebar() {
+    const location = useLocation();
+    const [tab, setTab] = useState('');
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const tabFromUrl = urlParams.get('tab');
+        // if tab is not null then execute below condition.
+        if (tabFromUrl) {
+            setTab(tabFromUrl);
+        }
+    }, [location.search]);
+    return (
+        <Sidebar className='w-full md:w-56'>
+            <Sidebar.Items>
+                <Sidebar.ItemGroup>
+                    <Link to="/dashboard?tab=profile">
+                    <Sidebar.Item active={tab=='profile'} icon={HiUser} label={"user"} labelColor="dark">
+                        Profile
+                    </Sidebar.Item>
+                    </Link>
+                    <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
+                        Sign Out
+                    </Sidebar.Item>
+                </Sidebar.ItemGroup>
+            </Sidebar.Items>
+        </Sidebar>
+    )
+};
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -50,11 +50,11 @@ const userSlice = createSlice({
     //     state.loading = false;
     //     state.error = action.payload;
     //   },
-    //   signoutSuccess: (state) => {
-    //     state.currentUser = null;
-    //     state.error = null;
-    //     state.loading = false;
-    //   },
+      signoutSuccess: (state) => {
+        state.currentUser = null;
+        state.error = null;
+        state.loading = false;
+      },
     },
   });
   
